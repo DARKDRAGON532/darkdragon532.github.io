f0ca3e423c5be863a741fcbe2bbc6c5b576b6503
DARKDRAGON532 <69623818+DARKDRAGON532@users.noreply.github.com>
2021-05-13 16:22:12 +0530

    deploy

--- src/main.js
+++ src/main.js
@@ -4,6 +4,23 @@ const about = document.querySelector("#about")
 const projects = document.querySelector("#projects")
 const about_content = document.querySelector("#about-content")
 const projects_content = document.querySelector("#projects-content")
+let terminal_string = "";
+
+const updateTerminal = () => {
+    document.querySelector(".text").innerHTML = terminal_string;
+}
+
+document.addEventListener("keypress", e => {
+    if (e.key === "Enter") return
+    terminal_string += e.key
+    updateTerminal()
+})
+
+document.addEventListener("keydown", e => {
+    if (e.key === "Escape") terminal_string = ""
+    else if (e.key === "Backspace") terminal_string = terminal_string.slice(0, -1)
+    updateTerminal()
+})
 
 about.addEventListener('click', () => {
     const about_box = new WinBox({
